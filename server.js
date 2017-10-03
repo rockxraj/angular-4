59e5e3aa65870950e85197a8b848c7aeb7b0c217
Rajendra <ttt@tttui-Mac-mini.local>
2017-10-03 15:06:42 +0530

    fixed issue 3 time

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/account'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8000);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/account/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 console.log('console listening');
